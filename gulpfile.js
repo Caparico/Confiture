1a21e8b1159b6430781beec6eeb4d2bf75839fea
Kobe <kobi.birdman@gmail.com>
2016-08-13 17:57:46 +0300

    Configure gulp tasks
    Add gulp to compile Sass files, watch and throw errors

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,34 @@
 require('es6-promise').polyfill();
-var gulp = require('gulp');
-// default task
-gulp.task('default');
+var gulp          = require('gulp');
+var sass          = require('gulp-sass');
+var autoprefixer  = require('gulp-autoprefixer');
+var rtlcss        = require('gulp-rtlcss');
+var rename        = require('gulp-rename');
+var plumber       = require('gulp-plumber');
+var gutil         = require('gulp-util');
+
+// What happens when an error is detected
+var onError = function (err) {
+    console.log('An error occurred:', gutil.colors.magenta(err.message));
+    gutil.beep();
+    this.emit('end');
+};
+
+// The gulp SASS task pipeline
+gulp.task('sass', function() {
+    return gulp.src('./sass/*.scss')
+        .pipe(plumber({ errorHandler: onError })) // Output colorful errors on console
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(gulp.dest('./'))              // Output LTR stylesheets (style.css)
+
+        .pipe(rtlcss())                     // Convert to RTL
+        .pipe(rename({basename: 'rtl'}))  // Rename to rtl.css
+        .pipe(gulp.dest('./'));             // Output RTL stylesheets (rtl.css)
+});
+
+gulp.task('watch', function() {
+    gulp.watch('./sass/**/*.scss', ['sass']);
+});
+
+gulp.task('default', ['sass', 'watch']);
